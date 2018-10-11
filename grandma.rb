73926ae88cdbddb942abce0513b594a3a752db4d
Talisa Erazo <talisaerazo@gmail.com>
2018-10-11 18:21:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,8 @@ def speak_to_grandma(phrase) {
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
 }
-  else phrase == "I LOVE YOU GRANDMA!"
+  else (phrase == "I LOVE YOU GRANDMA!") {
     return "I LOVE YOU TOO PUMPKIN!"
   end
+}
 end
