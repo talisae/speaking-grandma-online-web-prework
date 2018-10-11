dce22f798eba2b0534599009ed1068d802a94dac
Talisa Erazo <talisaerazo@gmail.com>
2018-10-11 18:22:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ phrase = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
